68cd123214089b75b59958f189d66d26dfcded76
feruzteame <teamefitwi@gmail.com>
2022-05-24 10:50:41 +0200

    create user using form, add some style and create route

---
+++ crud/src/Components/Create_user/Create.js
@@ -0,0 +1,100 @@
+
+import React, { useState,useRef } from 'react';
+import {v4 as uuid} from "uuid";
+import "./style.scss"
+
+function Create() {
+
+  // set value
+  const name = useRef();
+  const title = useRef();
+  const body = useRef();
+
+  // set empty Array
+  const [list, setUser] = useState([]);
+   
+ // add function user
+  const AddUser = () => {
+
+    // if the value is not empty
+      if (list !== "") { 
+
+        // saved current value(new value)
+        const nameValue = name.current.value;
+        const titleValue = title.current.value;
+        const bodyValue = body.current.value;
+
+        // put the data in the form of object
+        const listObject = {
+          id: name.current.getAttribute('data-key'),
+          name: nameValue,
+          title: titleValue,
+          body: bodyValue
+        }
+
+        // add the new user with we already have user and crete new array for all user
+        const newUser =  [...list, listObject];
+
+        // then set the new array 
+        setUser(newUser)
+        console.log(newUser)
+        
+        // make the space(input form) empty after submit
+        name.current.value = "";
+        title.current.value = "";
+        body.current.value = "";
+        return newUser
+
+        // if the input is empty send some message
+        }else{
+        alert("Please add some text before add")
+      }
+    }
+    
+
+   return (
+       <>
+      <div className="form">
+          <fieldset>
+                <legend>Add user</legend>
+                <label htmlFor="name">Name</label> <br/>
+               <input
+                    type="text"
+                    name="list"
+                    data-key={uuid()}
+                    id="text"
+                    ref={name}
+                    placeholder="Put your name..."
+                />
+                <br/> 
+                <label htmlFor="name">Title</label> <br/>
+               <input
+                    type="text"
+                    name="list"
+                    data-key={uuid()}
+                    id="text"
+                    ref={title}
+                    placeholder="Put your title..."
+                />
+                <br/> 
+                <label htmlFor="name">message</label> <br/>
+               <textarea
+                    type="text"
+                    name="list"
+                    data-key={uuid()}
+                    id="text"
+                    ref={body}
+                    placeholder="Put your message..."
+                />
+                <br/> 
+                <button type="submit" onClick={AddUser}>Add</button>
+            </fieldset>
+            
+      </div>
+      
+      </>
+       );
+       
+  }
+
+export default Create;
