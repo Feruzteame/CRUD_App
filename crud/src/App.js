1f4541df355e963e69b712fa22abb8133bf93c00
feruzteame <teamefitwi@gmail.com>
2022-05-24 17:24:28 +0200

    add some style and local-storage

--- crud/src/App.js
+++ crud/src/App.js
@@ -3,13 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Create from "./Components/Create_user/Create";
 
-
 const App = () => (
-
-    <Router>
-      
-        <Routes>
-                <Route exact path="/create" element={<Create/>} />
+   <Router>
+      <Routes>
+                <Route exact path="/" element={<Create/>} />
         </Routes>
     </Router>
 );
